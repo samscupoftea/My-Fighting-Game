7a86bc64ab11bf1654a9f33dbe2f3ff2ec0d2096
sam hockaday <samuel.hockaday@gmail.com>
2023-05-27 20:35:57 +0100

    Fixed query selector to target canvas h and W.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext(2d);
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
 canvas.width = 1024
 canvas.height = 576
+
+console.log('Hello world');
