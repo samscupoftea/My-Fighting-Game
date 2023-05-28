957047e61e6ac24bc090b0029ad6b9f534b5a7f6
sam hockaday <samuel.hockaday@gmail.com>
2023-05-28 20:37:37 +0100

    Fixed key up controlling of sprite.

--- index.js
+++ index.js
@@ -61,12 +61,29 @@ const enemy = new Sprite({
 
 console.log(player)
 
+
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
+
+    if (keys.a.pressed) {
+        player.velocity.x = - 1
+    } else if (keys.d.pressed) {
+        player.velocity.x = 1
+
+
+    }
 
 }
 
@@ -76,10 +93,10 @@ animate()
 window.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'd':
-            player.velocity.x = 1
+            keys.d.pressed = true
             break
         case 'a':
-            player.velocity.x = -1
+            keys.a.pressed = true
             break
     }
     console.log(event.key);
@@ -89,10 +106,10 @@ window.addEventListener('keydown', (event) => {
 window.addEventListener('keyup', (event) => {
     switch (event.key) {
         case 'd':
-            player.velocity.x = 0
+            keys.d.pressed = false
             break
         case 'a':
-            player.velocity.x = 0
+            keys.a.pressed = false
             break
     }
     console.log(event.key);
